61f65a6244ca0e6ae2bbbc59bfd994ff10db2f8f
Thomas Roulin <thomasroulinCH@gmail.com>
2016-06-09 15:52:57 +0200

    Path smoothing with Catmull Rom

--- js/CameraTracking.js
+++ js/CameraTracking.js
@@ -14,10 +14,10 @@ function CameraTracking(camera){
     this.path = [];
     this.spline = undefined;
     this.IS_RUNNING = false;
-    this.IS_ROTATING = false;
 
     this.current_id = 0;
     this.speed = CAMERA_DEFAULT_SPEED;
+    this.timer = 0.0;
 }
 
 /**
@@ -27,7 +27,7 @@ function CameraTracking(camera){
 */
 CameraTracking.prototype.set_path = function(path){
     this.path = path;
-    this.spline = new THREE.Spline(this.path);
+    this.spline = new THREE.CatmullRomCurve3(this.path);
 };
 
 /**
@@ -35,6 +35,7 @@ CameraTracking.prototype.set_path = function(path){
 */
 CameraTracking.prototype.start = function(){
     this.IS_RUNNING = true;
+    this.timer = 0.0;
     this.current_id = 0;
     this.camera.position.copy(this.path[this.current_id]);
 };
@@ -44,7 +45,6 @@ CameraTracking.prototype.start = function(){
 */
 CameraTracking.prototype.stop = function(){
     this.IS_RUNNING = false;
-    this.IS_ROTATING = false;
 };
 
 
@@ -57,33 +57,14 @@ CameraTracking.prototype.update = function(delta){
     if(!this.IS_RUNNING)
         return;
 
-    // init the node we come from and the node we are going to
-    var v_from = this.path[this.current_id];
-    var v_to = this.path[this.current_id + 1];
+    this.timer += delta;
+    var t_camera = (this.speed * this.timer) / this.spline.getLength();
+    var t_look = t_camera + 0.01;
 
-    // Computing
-    var v_dir = v_to.clone().sub(this.camera.position).normalize();
-    var v_shifting = v_dir.multiplyScalar(delta * this.speed);
-
-    if(this.IS_ROTATING)
-    {
-        var cam = this.camera.clone();
-        cam.lookAt(v_to);
-        this.camera.quaternion.slerp(cam.quaternion, 0.01);
-    }
-    else
-    {
-        // Setting camera position and direction
-    this.camera.quaternion.normalize();
-        this.camera.lookAt(v_to);
-        this.camera.position.add(v_shifting);
-
-        if(this.camera.position.distanceTo(v_to) < 10)
-        {
-            this.current_id++;
-            // this.IS_ROTATING = true;
-        }
-    }
+    var p_camera = this.spline.getPointAt( t_camera );
+    var p_look = this.spline.getPointAt( t_look );
+    this.camera.position.copy( p_camera );
+    this.camera.lookAt( p_look );
 };
 
 /**
