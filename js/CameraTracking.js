45256068269310c1739b1f2efaedaf7eade68b01
Thomas Roulin <thomasroulinCH@gmail.com>
2016-06-09 13:25:32 +0200

    Basic rotation mechanic. Not fully working though.

--- js/CameraTracking.js
+++ js/CameraTracking.js
@@ -12,7 +12,9 @@ var CAMERA_DEFAULT_SPEED = 50;
 function CameraTracking(camera){
     this.camera = camera;
     this.path = [];
+    this.spline = undefined;
     this.IS_RUNNING = false;
+    this.IS_ROTATING = false;
 
     this.current_id = 0;
     this.speed = CAMERA_DEFAULT_SPEED;
@@ -25,23 +27,24 @@ function CameraTracking(camera){
 */
 CameraTracking.prototype.set_path = function(path){
     this.path = path;
+    this.spline = new THREE.Spline(this.path);
 };
 
 /**
 * Set the camera mode to running.
 */
-CameraTracking.prototype.start = function(speed = CAMERA_DEFAULT_SPEED){
+CameraTracking.prototype.start = function(){
     this.IS_RUNNING = true;
     this.current_id = 0;
     this.camera.position.copy(this.path[this.current_id]);
-    this.speed = speed;
 };
 
 /**
-* Set the camera mode to idle.
+* Stop and reset the camera.
 */
 CameraTracking.prototype.stop = function(){
     this.IS_RUNNING = false;
+    this.IS_ROTATING = false;
 };
 
 
@@ -62,12 +65,32 @@ CameraTracking.prototype.update = function(delta){
     var v_dir = v_to.clone().sub(this.camera.position).normalize();
     var v_shifting = v_dir.multiplyScalar(delta * this.speed);
 
-    // Setting camera position and direction
-    this.camera.lookAt(v_to);
-    this.camera.position.add(v_shifting);
-
-    if(this.camera.position.distanceTo(v_to) < 10)
+    if(this.IS_ROTATING)
     {
-        this.current_id++;
+        var cam = this.camera.clone();
+        cam.lookAt(v_to);
+        this.camera.quaternion.slerp(cam.quaternion, 0.01);
     }
+    else
+    {
+        // Setting camera position and direction
+    this.camera.quaternion.normalize();
+        this.camera.lookAt(v_to);
+        this.camera.position.add(v_shifting);
+
+        if(this.camera.position.distanceTo(v_to) < 10)
+        {
+            this.current_id++;
+            // this.IS_ROTATING = true;
+        }
+    }
+};
+
+/**
+* Set the camera velocity
+*
+* @param {Number} The speed to set
+*/
+CameraTracking.prototype.set_speed = function (speed) {
+    this.speed = speed;
 };
