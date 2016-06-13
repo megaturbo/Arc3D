f7cdfeae4709f8dd30af9a757e2017793b4b61b2
Thomas Roulin <thomasroulinCH@gmail.com>
2016-06-13 16:14:21 +0200

    New model versions

---
+++ models/multipart/window_0.js
@@ -0,0 +1,19 @@
+{
+
+"metadata":
+{
+"sourceFile": "hearc.max",
+"generatedBy": "3ds max ThreeJSExporter",
+"formatVersion": 3.1,
+"vertices": 8,
+"normals": 12,
+"colors": 0,
+"uvs": 10,
+"triangles": 12,
+"materials": 1
+},
+
+"materials": [
+{
+"DbgIndex" : 0,
+"DbgName"  : "Material #63",
