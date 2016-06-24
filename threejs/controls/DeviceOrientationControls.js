d751826a01a8e0c76a13e29f5a3dead048452fec
Thomas Roulin <thomasroulinCH@gmail.com>
2016-06-24 10:32:08 +0200

    Computing compass heading

--- threejs/controls/DeviceOrientationControls.js
+++ threejs/controls/DeviceOrientationControls.js
@@ -90,10 +90,41 @@ THREE.DeviceOrientationControls = function ( object ) {
 		var gamma  = scope.deviceOrientation.gamma ? THREE.Math.degToRad( scope.deviceOrientation.gamma ) : 0; // Y''
 		var orient = scope.screenOrientation       ? THREE.Math.degToRad( scope.screenOrientation       ) : 0; // O
 
+        console.log(this.compassHeading(alpha, beta, gamma));
 
 		setObjectQuaternion( scope.object.quaternion, alpha, beta, gamma, orient );
 
 	};
+
+    this.compassHeading = function( alpha, beta, gamma ) {
+
+        var _x = beta; // beta value
+        var _y = gamma; // gamma value
+        var _z = alpha; // alpha value
+
+        var cX = Math.cos( _x );
+        var cY = Math.cos( _y );
+        var cZ = Math.cos( _z );
+        var sX = Math.sin( _x );
+        var sY = Math.sin( _y );
+        var sZ = Math.sin( _z );
+
+        // Calculate Vx and Vy components
+        var Vx = - cZ * sY - sZ * sX * cY;
+        var Vy = - sZ * sY + cZ * sX * cY;
+
+        // Calculate compass heading
+        var compassHeading = Math.atan( Vx / Vy );
+
+        // Convert compass heading to use whole unit circle
+        if( Vy < 0 ) {
+            compassHeading += Math.PI;
+        } else if( Vx < 0 ) {
+            compassHeading += 2 * Math.PI;
+        }
+
+        return compassHeading * ( 180 / Math.PI ); // Compass Heading (in degrees)
+    };
 
 	this.dispose = function () {
 
